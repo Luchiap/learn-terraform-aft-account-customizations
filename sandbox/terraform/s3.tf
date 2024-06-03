f50e4755e28f4b12c47e1c8356a98b4f12885ece
Luigi <luchiap@amazon.com>
2024-06-03 13:25:10 -0500

    bello

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -2,11 +2,8 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
 }
 resource "aws_ec2_transit_gateway" "AFTtransitgateway3" {
-  description = "example transit gateway deployed with global customizations3"
+  description = "example transit gateway deployed with account cust"
 }
-resource "aws_ec2_transit_gateway" "AFTtransitgateway4" {
-  description = "example transit gateway deployed with global customizations3"
-}
+
