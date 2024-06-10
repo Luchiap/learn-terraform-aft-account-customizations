f2aca71a3013f78f07a9c6b73682e115c3a49e70
Luigi <luchiap@amazon.com>
2024-06-10 14:24:45 -0500

    fdsf

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,12 +1,11 @@
 data "aws_caller_identity" "current" {}
-provider "aws" {
-  alias   = "us_east_2"
-  region  = "us-east-2"
-}
-
 resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
 }
 resource "aws_ec2_transit_gateway" "AFTtransitgateway3" {
   description = "example transit gateway deployed with account cust"
+}
+resource "aws_ec2_transit_gateway" "AFTtransitgateway4" {
+  provider = aws.us_east_2
+  description = "example transit gateway 4deployed with account cust"
 }
