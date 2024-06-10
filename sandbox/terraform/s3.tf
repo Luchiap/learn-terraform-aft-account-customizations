bfd35d29d3c633de20846b3f3af9cacdb1036635
Luigi <luchiap@amazon.com>
2024-06-10 13:45:57 -0500

    modified region

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,9 +1,11 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
+  provider = aws.west
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
 }
 resource "aws_ec2_transit_gateway" "AFTtransitgateway3" {
+  provider = aws.west
   description = "example transit gateway deployed with account cust"
 }
 
