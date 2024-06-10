07ed50d1359c1a911267c20ffd80038c6df3bce8
Luigi <luchiap@amazon.com>
2024-06-10 16:23:08 -0500

    cs

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -9,3 +9,7 @@ resource "aws_ec2_transit_gateway" "AFTtransitgateway4" {
   provider = aws.us_east_2
   description = "example transit gateway 4deployed with account cust"
 }
+resource "aws_ec2_transit_gateway" "AFTtransitgateway5" {
+  provider = aws.eu_central_2
+  description = "example transit gateway 5deployed with account cust"
+}
