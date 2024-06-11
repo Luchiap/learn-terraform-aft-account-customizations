4765ac4941dc3fc1c139d7efb374ca728ccaf3a9
Luigi <luchiap@amazon.com>
2024-06-11 10:45:40 -0500

    dasd

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,20 +1,21 @@
 data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+  bucket = "aft-sandbox-gfdgdfgfdgfdgdfgfdgfd"
+  provider = aws.us_east_2
 }
-# resource "aws_ec2_transit_gateway" "AFTtransitgateway4" {
-#   provider = aws.us_east_2
-#   description = "example transit gateway 4deployed with account cust"
-# }
-# resource "aws_ec2_transit_gateway" "AFTtransitgateway8" {
-#   provider = aws.eu_central_2
-#   description = "example transit gateway 8deployed with account cust"
-# }
-# resource "aws_s3_bucket" "sandbox_bucket" {
-#   bucket = "aft-sandboxdfdsfg-${data.aws_caller_identity.current.account_id}"
-#   provider = aws.us_east_2
-# }
-# resource "aws_s3_bucket" "sandbox_bucket" {
-#   bucket = "aft-sandboxdfdsfgfdsf-${data.aws_caller_identity.current.account_id}"
-#   provider = aws.eu_central_2
-# }
+resource "aws_ec2_transit_gateway" "AFTtransitgateway4" {
+  provider = aws.us_east_2
+  description = "example transit gateway 4deployed with account cust"
+}
+resource "aws_ec2_transit_gateway" "AFTtransitgateway8" {
+  provider = aws.eu_central_2
+  description = "example transit gateway 8deployed with account cust"
+}
+resource "aws_s3_bucket" "sandbox_bucket" {
+  bucket = "aft-sandboxdfdsfgtrtyrtrtrt-${data.aws_caller_identity.current.account_id}"
+  provider = aws.us_east_2
+}
+resource "aws_s3_bucket" "sandbox_bucket" {
+  bucket = "aft-sandboxdfdsfgfdsffsdfsdf-${data.aws_caller_identity.current.account_id}"
+  provider = aws.eu_central_2
+}
