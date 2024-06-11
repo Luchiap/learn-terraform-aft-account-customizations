e2cbfb176f6031f88b59d6f51123e23146f81dfe
Luigi <luchiap@amazon.com>
2024-06-11 09:07:05 -0500

    dasdsad

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -9,7 +9,11 @@ resource "aws_ec2_transit_gateway" "AFTtransitgateway4" {
   provider = aws.us_east_2
   description = "example transit gateway 4deployed with account cust"
 }
-resource "aws_ec2_transit_gateway" "AFTtransitgateway5" {
+resource "aws_ec2_transit_gateway" "AFTtransitgateway8" {
   provider = aws.eu_central_2
-  description = "example transit gateway 5deployed with account cust"
+  description = "example transit gateway 8deployed with account cust"
+}
+resource "aws_s3_bucket" "sandbox_bucket" {
+  bucket = "aft-sandboxdfdsf-${data.aws_caller_identity.current.account_id}"
+  provider = aws.eu_central_2
 }
